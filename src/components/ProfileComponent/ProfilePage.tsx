61f98bf50ba68cb106cac1f9506e070d2862a554
Jennifer Córdoba <sofiac27@gmail.com>
2024-03-06 19:15:41 -0600

    image github and others changes
    image github and others changes

--- src/components/ProfileComponent/ProfilePage.tsx
+++ src/components/ProfileComponent/ProfilePage.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Profile from '../../Pages/Profile';
-import { UserProfile } from './profileTypes';
+import React from "react";
+import Profile from "../../Pages/Profile";
+import { UserProfile } from "./profileTypes";
 
 const ProfilePage: React.FC = () => {
   const [currentUser, setCurrentUser] = React.useState<UserProfile>({
-    name: 'Nombre del Usuario',
-    bio: 'Descripción del Usuario',
-    avatar: 'URL de la imagen del avatar',
-    editableName: '',
-    editableBio: '',
-    editableAvatar: '',
+    name: "Nombre del Usuario",
+    bio: "Descripción del Usuario",
+    avatar: "URL de la imagen del avatar",
+    editableName: "",
+    editableBio: "",
+    editableAvatar: "",
   });
 
   const handleUserDataChange = (newUserData: UserProfile) => {
@@ -18,7 +18,7 @@ const ProfilePage: React.FC = () => {
 
   return (
     <div className="profile-page">
-      <h1>Perfil de Usuario</h1>
+      <h1 className="Main">Perfil de Usuario</h1>
       <Profile user={currentUser} onUserDataChange={handleUserDataChange} />
     </div>
   );
